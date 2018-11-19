d4a680e832349ca0341e17d3a6335c27914fbfff
Bingmang <ustbbingmang@foxmail.com>
2018-11-19 15:11:43 +0800

    [add] imagecut with multiprocess

--- opencv/imagecut.py
+++ opencv/imagecut.py
@@ -1,11 +1,13 @@
 import argparse
 import os
 import cv2
+import multiprocessing
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--dir', required=True, help='folder where your data is in')
 parser.add_argument('--out', default='imagecut/', help='folder to output cut images')
 parser.add_argument('--size', type=int, default=200, help='the height / width of the output cut image')
+parser.add_argument('--rename', type=bool, default=False, help='auto rename to 000001.jpg - 999999.jpg')
 
 opt = parser.parse_args()
 print(opt)
@@ -32,12 +34,20 @@ def cut_image(file, size, outdir):
     for y in range(0, height, size):
         for x in range(0, width, size):
             if x + size > width or y + size > height:
-                print(x, y, width, height)
                 continue
             # if you want to change the cut direction, just change the code to img[x : x + size, y : y + size]
             cv2.imwrite('%s/%s-%s%s' % (outdir, basename, cut_num, extension), img[y : y + size, x : x + size]) 
             cut_num += 1
     print('image: [%s] cuts out %s images.' % (file, cut_num))
+
+def rename(outdir):
+    # it will be unordered
+    num = 0
+    extension = '.jpg'
+    for file in os.listdir(outdir):
+        os.rename(outdir + file, '%s%06d%s' % (outdir, num, extension))
+        num += 1
+    return
 
 def main():
     mkoutdir(opt.out)
@@ -45,8 +55,14 @@ def main():
     if not file_list:
         print('find nothing in your data folder: %s' % opt.dir)
         return
+    # cut images with multiple process
+    pool = multiprocessing.Pool(processes=multiprocessing.cpu_count())
     for file in file_list:
-        cut_image(file, opt.size, opt.out)
+        pool.apply_async(cut_image, (file, opt.size, opt.out))
+    pool.close()
+    pool.join()
+    if opt.rename:
+        rename(opt.out)
 
 if __name__ == '__main__':
     main()
